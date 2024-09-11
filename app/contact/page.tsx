82d0a4622687049a40e8b2ddec6f5383f88cfdb6
Petro Lesapiti <petrolesapiti@gmail.com>
2024-09-12 01:20:32 +0300

    Factor code and remove unneeded files

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Contact } from "@/components/contact";
-export default function contactPage() {
-  return (
-    <section className="w-full py-[130px] px-10 flex flex-row">
-      <Contact />
-    </section>
-  );
-}
--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,9 @@
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
-import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
   children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
 }
 
 export function Providers({ children }: ProvidersProps) {
