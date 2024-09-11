ba2142350c53a03887dd52c45068e6cf1979a838
Petro Lesapiti <petrolesapiti@gmail.com>
2024-09-12 01:07:18 +0300

    The test code folder

--- app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Hero } from "@/components/backup";
-export default function TestPage() {
-  return (
-    <div className="relative flex flex-row items-center justify-stretch w-full py-[130px] bg-[#10393b] px-10">
-      <Hero />
-    </div>
-  );
-}
